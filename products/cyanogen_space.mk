0483f22768568c765d56f8a5096558dffbb329ac
fredvj <fredvjupiter@googlemail.com>
2012-02-18 18:35:39 +0100

    Changed the order of including overlays - GSM probably made the built a phone, not a tablet
    Change-Id: I812c86726e0868cee54f813786fc10b47b42893e

--- products/cyanogen_space.mk
+++ products/cyanogen_space.mk
@@ -1,11 +1,11 @@
-# Inherit AOSP device configuration for blade.
-$(call inherit-product, device/odys/space/device_space.mk)
-
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+# Inherit device configuration for Odys Space
+$(call inherit-product, device/odys/space/device_space.mk)
 
 #
 # Setup device specific product configuration.
@@ -15,7 +15,7 @@ PRODUCT_BRAND := odys
 PRODUCT_DEVICE := space
 PRODUCT_MODEL := Space
 PRODUCT_MANUFACTURER := Odys
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=space BUILD_ID=2012-01 BUILD_FINGERPRINT=space:2.3.7/2012-01/release-keys PRIVATE_BUILD_DESC="Add desc. here"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=space BUILD_ID=2012-02 BUILD_FINGERPRINT=space:2.3.7/2012-02/release-keys PRIVATE_BUILD_DESC="Add desc. here"
 
 PRODUCT_PACKAGE_OVERLAYS += \
 	vendor/cyanogen/overlay/tablet
